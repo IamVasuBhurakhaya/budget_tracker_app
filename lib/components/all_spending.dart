053c4a6bb3c8fa64dc949835e3cbb51977f0f55d
vasuvibingprivetly <bhurakhyavasu@gmail.com>
2025-01-02 15:17:19 +0530

    Initial Commit

---
+++ lib/components/all_spending.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllSpending extends StatelessWidget {
+  const AllSpending({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'All Category',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
